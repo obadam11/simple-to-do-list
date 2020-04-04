4875f16c4b87cb4eaa1acc40472f3f9455f75b7c
obadam11 <obada.mik.ab@gmail.com>
2020-04-04 20:45:14 +0300

    Added the todo object, made the add task button function, continuing to addto the class

--- todo.py
+++ todo.py
@@ -21,22 +21,78 @@ class App(object):
     
     def stay_running(self):
         self.screen.mainloop()
-    
+
+
     def entry_bar(self, x, y, width=180):
+        global eb
         eb = tk.Entry(self.screen, borderwidth=5)
         eb.place(x=x, y=y, width=width)
     
-    def but(self, background, x, y, width ,text):
-        button = tk.Button(self.screen, text=text, bg=background, font=("Corier", 10, "bold"), borderwidth=5)
-        button.place(x=x, y=y, width=width)
-    
+    def get_entry(self):
+        global eb
+        return eb.get()
+
+
     def list_box(self, x, y, width, height):
+        global lb
         lb = tk.Listbox(self.screen)
         lb.place(x=x, y=y, width=width, height=height)
     
+    def add_to_list_box(self, inserting):
+        global lb
+        lb.insert("end", inserting)
+    
+
+    def but(self, background, x, width ,text, cmd=None, y=85):
+        b = tk.Button(self.screen, text=text, bg=background, font=("Corier", 8, "bold"), borderwidth=5, command=cmd)
+        b.place(x=x, y=y, width=width)
+    
+
+    
+
+    
     def on_screen(self, x, y, txt, background, foreground="black"):
-        t = tk.Label(self.screen, text=txt, bg=background, fg=foreground)
+        t = tk.Label(self.screen, text=txt, bg=background, fg=foreground, font=("Courier", 14, "bold"))
         t.place(x=x, y=y)
 
 
 
+todo = App(500, 500)
+todo.draw("To-Do List", color=yellow)
+todo.on_screen(170, 10, "To Do list app", yellow)
+todo.entry_bar(160, 50)
+todo.list_box(10, 140, 480, 340)
+
+# Add task
+
+def add_task():
+    getting = todo.get_entry()
+    todo.add_to_list_box(getting)
+
+
+todo.but(dark_yellow, 160, 75, "Add Task", add_task)
+
+# Remove task
+
+def remove_task():
+    pass
+
+todo.but(dark_yellow, 260, 75, "Rmv Task", remove_task)
+
+# Remove all
+
+def remove_all():
+    pass
+
+todo.but(dark_yellow, 360, 75, "Rmv all", remove_all)
+
+# order
+def order():
+    pass
+
+todo.but(dark_yellow, 60, 75, "order", order)
+
+
+
+
+todo.stay_running()
