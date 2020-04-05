61a7281738ee1e3a5ad8093847aa8727c5a89787
obadam11 <obada.mik.ab@gmail.com>
2020-04-05 18:41:17 +0300

    Added the docstrings and made the deleting functionality in both classmethods

--- todo.py
+++ todo.py
@@ -1,3 +1,4 @@
+""" Simple todo list GUI python app """
 import tkinter as tk
 import datetime
 from tkinter import messagebox
@@ -7,14 +8,14 @@ yellow = "#F7FF45"
 dark_yellow = "#AFB438"
 
 class App(object):
-
+    """ Making the app class """
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.screen = "root"
 
     def draw(self, title, color="#FFFFFF"):
-        # global root
+        """ Drawing the the window and giving some details """
         self.screen = tk.Tk()
         self.screen.geometry(f"{self.width}x{self.height}")
         self.screen.config(bg=color)
@@ -24,19 +25,22 @@ class App(object):
         self.screen.mainloop()
 
     def list_box(self, x, y, width, height):
+        """ Making a list box and placing it (To place the tasks in it) """
         global lb
         lb = tk.Listbox(self.screen)
         lb.place(x=x, y=y, width=width, height=height)
 
 
     def entry_bar(self, x, y, width=180):
+        """ Making an entry bar and placing it """
         global eb
         eb = tk.Entry(self.screen, borderwidth=5)
         eb.place(x=x, y=y, width=width)
     
-    
+    """ Making 4 classmethods for each button """
     @classmethod
     def get_entry(cls):
+        """ Getting input from entry bar, displaying it in the list boc, and writing it into the file. """
         global eb, lb
         content =  eb.get()
         print(content)
@@ -53,16 +57,33 @@ class App(object):
 
     @classmethod
     def remove_last(cls):
+        """ Delete the last task added from the listbox """
         global lb
         lb.delete("end")
+
+        with open("todo.txt") as f:
+            lines = f.readlines()
+        lines = lines[:-1]
+        print(lines)
+        with open("todo.txt", "w") as f_w:
+            f_w.writelines(lines)
+
+        
     
     @classmethod
     def remove_all(cls):
+        """ Remove all tasks from the listbox and the todo.txt file file. """
         global lb
         lb.delete(0, "end")
+        try:
+            with open("todo.txt", "w") as f_erase:
+                f_erase.write("")
+        except FileNotFoundError:
+            print("File not Found")
 
     @classmethod
     def most_important(cls):
+        """ Inserting an important task, and writing ot to the file. """
         global lb, eb
         content = f"{eb.get()}                         IMPORTANT"
         lb.insert(0, content)
@@ -73,12 +94,12 @@ class App(object):
                 f_object.write(content)
         except FileNotFoundError:
             print("This file is not found")
-            
 
 
     
 
     def but(self, background, x, width ,text, cmd=None, y=85):
+        """ Button object, since we have 4 buttons. """
         b = tk.Button(self.screen, text=text, bg=background, font=("Corier", 8, "bold"), borderwidth=5, command=cmd)
         b.place(x=x, y=y, width=width)
     
@@ -87,10 +108,12 @@ class App(object):
 
     
     def on_screen(self, x, y, txt, background, foreground="black"):
+        """ Putting text on screen """
         t = tk.Label(self.screen, text=txt, bg=background, fg=foreground, font=("Courier", 14, "bold"))
         t.place(x=x, y=y)
     
     def present_file(self):
+        """ Reads the todo.txt file when the program is opened. """
         global lb
         try:
             with open("todo.txt") as f_read:
@@ -103,7 +126,7 @@ class App(object):
 
 
 
-
+""" Making an instance of the app class and using its methods. """
 todo = App(500, 500)
 todo.draw("To-Do List", color=yellow)
 todo.on_screen(170, 10, "To Do list app", yellow)
